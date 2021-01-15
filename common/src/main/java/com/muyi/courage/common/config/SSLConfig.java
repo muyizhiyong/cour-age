bb258bd7b165338711dc85755bd8034ee612eaba
yangzhiyong <muyi_zhiyong@163.com>
2021-01-15 16:51:05 +0800

    增加支持https，http&https双端口

---
+++ common/src/main/java/com/muyi/courage/common/config/SSLConfig.java
@@ -0,0 +1,32 @@
+package com.muyi.courage.common.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 杨志勇
+ * @date 2021-01-15 16:44
+ */
+@Configuration
+public class SSLConfig {
+
+    @Value("${server.non-ssl-port}")
+    int port;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(port);
+        return connector;
+    }
+}
